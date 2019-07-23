26af244f479614ad0fbcecabeb0c18b428c6f8ab
Kenny B <kenneth.backus@gmail.com>
2019-07-22 20:03:39 -0400

    added CustomPropeller and PropellerStage

---
+++ Assets/SkyRogueModTool/Scripts/CustomPropeller.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SkyRogueModTool
+{
+    public class CustomPropeller : MonoBehaviour
+    {
+        [Tooltip("CAN BE 'NONE'. This is the model which will be shown when the engine is turned off, such as on the carrier menu.")]
+        public Transform staticPropMesh;
+        [Tooltip("A propeller can change its visualization based on the aircraft's current throttle. This lets you transition from a solid prop to a transparent blur effect. If you want to keep things simple, just define a single prop stage.")]
+        public PropellerStage[] stages;
+    }
+}
---
+++ Assets/SkyRogueModTool/Scripts/PropellerStage.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class PropellerStage
+{
+    [Tooltip("The visible component of this prop stage")]
+    public Transform propMesh;
+    [Tooltip("Throttle at which this prop stage will be chosen. If you want to keep things simple, just define only one prop stage and set this to 0.5.")]
+    [Range(0.5f, 1.5f)]
+    public float beginThrottle = 0.5f;
+    [Tooltip("At minimum throttle, the 'speed' will be multiplied by this amount")]
+    [Range(-2f, 2f)]
+    public float minSpin = -1f;
+    [Tooltip("At maximum throttle, the 'speed' will be multiplied by this amount")]
+    [Range(-2f, 2f)]
+    public float maxSpin = 0.1f;
+    [Tooltip("Base rotation speed of this prop stage")]
+    public float speed = 1000f;
+}
